a3e05742fdb8c88011cf976659153bf5adc5ae71
Jdog2001 <134169267+JoshHill1@users.noreply.github.com>
2024-07-09 10:53:29 -0700

    progress on styling

--- script.js
+++ script.js
@@ -26,30 +26,3 @@ window.addEventListener("scroll", () => {
 
   lastScrollTop = scrollTop;
 });
-
-// making text of the main header to be the same as the background color when hovered over
-document.addEventListener("DOMContentLoaded", (event) => {
-  // Get the main header element
-  const mainHeader = document.querySelector(".main-header");
-
-  // Get the computed background color of the main header
-  const mainHeaderColor = window.getComputedStyle(mainHeader).backgroundColor;
-  // Get the filter value applied to the main header
-  const mainHeaderFilter = window.getComputedStyle(mainHeader, "::before").filter;
-
-  // Get the list of a tags
-  const aTags = document.querySelectorAll(".main-header a");
-
-  // Loop through the list of a tags and add an event listener for mouseover and mouseout
-  aTags.forEach((aTag) => {
-    aTag.addEventListener("mouseover", (event) => {
-      aTag.style.color = mainHeaderColor;
-      aTag.style.filter = mainHeaderFilter;
-    });
-    aTag.addEventListener("mouseout", (event) => {
-      aTag.style.color = "var(--accent-color)";
-      aTag.style.filter = "none";
-    });
-  });
-});
-
